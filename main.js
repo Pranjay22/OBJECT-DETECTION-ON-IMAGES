a553f411549dfb211b93ba97187fb1db7d0d493b
Pranjay22 <74549936+Pranjay22@users.noreply.github.com>
2021-07-17 08:38:56 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function bedroom(){
+    window.location="a.html";
+}
+
+function desk(){
+    window.location="d.html";
+}
+
+function fruitbasket(){
+    window.location="c.html";
+}
+
+function bottle(){
+    window.location="b.html";
+}
+
